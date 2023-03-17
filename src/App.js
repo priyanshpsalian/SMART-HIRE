3956bc628766c5f18f6e8b854575aa44c51f7d06
Priyansh Salian <79307288+priyanshpsalian@users.noreply.github.com>
2023-03-17 23:46:20 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Routes from "./Routes/index";
 
-//import Custom Style scss
+//import Custom Style scss in the below line
 import "./assets/scss/themes.scss";
 
 function App() {
